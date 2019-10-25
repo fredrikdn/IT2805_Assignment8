d27d0081e60d7699e1b4d42f8030e63da89cca86
Fredrik Dokka Nordstad <fredrikdn@gmail.com>
2019-10-25 13:48:20 +0200

    Assignment 8 finished Tasks completed, everything is ok  Changes to be committed: 	new file:   bar-chart-with-bars.html 	modified:   bar-chart-with-lines.html 	modified:   income-greater-than-500k.html 	new file:   questions.txt 	modified:   taxForms.js

--- taxForms.js
+++ taxForms.js
@@ -1,17 +1,24 @@
 const taxForms = [
-  {
-    realName: "Bruce Wayne",
-    income: 750000,
-    wealth: 300000
+    {
+        realName: "Bruce Wayne",
+        income: 750000,
+        wealth: 300000
   },
-  {
-    realName: "John Blake",
-    income: 440000,
-    wealth: 832000
+    {
+        realName: "John Blake",
+        income: 440000,
+        wealth: 832000
   },
-  {
-    realName: "Selina Kyle",
-    income: 640000,
-    wealth: 432000
+    {
+        realName: "Selina Kyle",
+        income: 640000,
+        wealth: 432000
   }
 ];
+
+//this filters heroes from taxForms who has income higher than 500000
+//and maps their real name
+var out = taxForms.filter(hero => hero.income >= 500000)
+.map(hero => hero.realName)
+
+console.log(out)
